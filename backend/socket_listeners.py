b239d875f76948898d625260b9b9c229f964d8c9
Dishank Mehta <mdishank@gmail.com>
2018-11-17 23:10:17 -0700

    adding models and login, register endpoints

--- backend/socket_listeners.py
+++ backend/socket_listeners.py
@@ -1,5 +1,7 @@
 import os
+import json
 from backend import create_app
+from models import User, Note
 from flask_socketio import  SocketIO, join_room, leave_room, emit
 
 env = os.environ.get('BACKEND_ENV', 'dev')
@@ -9,10 +11,15 @@ socketio = SocketIO(app)
 
 ROOMS = dict()
 
+CONNECT = 'CONNECT'
+TYPE_JOINED = 'JOINED'
+
+
 @socketio.on('connect')
 def connect():
     print("Connected to socket!")
-    emit('connect', {'data': "sample"})
+    payload = dict(type=CONNECT)
+    socketio.emit(CONNECT, payload)
 
 @socketio.on('disconnect')
 def disconnect():
@@ -22,3 +29,23 @@ def disconnect():
 def change_color(color):
     print("Color: ", color)
     socketio.emit('receive_change_color', color)
+
+@socketio.on('join_note')
+def join_note(data):
+    print("joining note")
+    note_id = data.get('note_id', None)
+    user_id = data.get('user_id', None)
+    if not note_id or not user_id:
+        return
+    # room = ROOMS.get(note_id, None)
+    # if not room:
+    #     ROOMS[note_id] = []
+    socketio.join_room(note_id)
+    payload = dict(type=TYPE_JOINED)
+    socketio.emit(TYPE_JOINED, payload)
+
+@socketio.on("leave_note")
+def leave_note(data):
+    print("leaving note")
+    note_id = data.get('note_id', None)
+    socketio.leave_room(note_id)
