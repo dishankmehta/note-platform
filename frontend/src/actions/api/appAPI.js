3a515436a98ce90d2356cb6522028e89cf0a0feb
mthakka2 <mthakka2@asu.edu>
2018-11-20 00:16:18 -0700

    Added Dashboard

--- frontend/src/actions/api/appAPI.js
+++ frontend/src/actions/api/appAPI.js
@@ -4,4 +4,8 @@ export default class appAPI {
 	static login(username, password) {
 		return API.post('/login', {username: username, password: password});
 	}
+
+	static sendNoteData(data) {
+		return API.post('/sendNoteData', data);
+	}
 }
--- frontend/src/containers/routes.js
+++ frontend/src/containers/routes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from '../components/LoginPage/LoginPage';
+import DashBoard from '../components/DashBoard/DashBoard';
+//import ColorPicker from '../components/ColorPicker/ColorPicker';
 
 const AppRoutes = () => (
 	<div>
 		<Switch>
 			<Route exact path="/" component={LoginPage}/>
+			<Route path="/dashboard" component={DashBoard}/>
 		</Switch>
 	</div>
+
+
 
 );
 
